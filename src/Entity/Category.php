9dc97690e8bc65c7dd97421bfeec4179c11f4572
ayaamamou00 <aya.amamou@esprit.tn>
2022-10-03 12:05:47 +0200

    db+entity

---
+++ src/Entity/Category.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\Column(length: 50)]
+    private ?string $ref = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $titre = null;
+
+    public function getRef(): ?string
+    {
+        return $this->ref;
+    }
+
+    public function setRef(string $ref): self
+    {
+        $this->ref= $ref;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+}
